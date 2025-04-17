b5d85f6fc4bce9bc884688cdf994c0d52d706fcb
anaskhan1126 <ananamaankhan1126@gmail.com>
2025-04-17 15:37:48 +0530

    function change

---
+++ function.js
@@ -0,0 +1,34 @@
+// function declaration 
+//============basic structure for function declaration===========
+// function demo(){
+//     console.log("hello");
+//     console.log(typeof(demo));
+// }
+//============function declaration using varible===========
+// let demo=function(){
+//     console.log(typeof(demo));
+
+// }
+//============function declaration using arrow===========
+// let demo=()=>{
+//     console.log("hello using arrow");
+// }
+// demo();
+// ===============function with parameters and without parameters============
+// let add=function(a,b){ 
+//     console.log(a+b);
+// }
+// add(3,8);
+// let mul=function(){ 
+//     let a=10;
+//     let b=89
+//     console.log(a*b);
+// }
+// mul();
+//============Function with default parameters================================
+let div=function(m,n=2){
+    console.log(m/n);
+
+}
+div(4)
+//=============
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+let demo=()=>{
+//     console.log("hello using arrow");
+// }
+// demo();
